a1d35c38904ec283b232ca30a6d899a37ad0ffc0
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-30 19:14:53 +0100

    add random array to click funct. & select arr. dom

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 // SELEZIONE degli elementi del DOM: bottone play, timer
 const playBtn = document.getElementById("play");
 const timerRunning = document.getElementById ("main-timer");
+const numbersToRemember = document.getElementById("numeri-da-ricordare");
 
 
 // FUNZIONE che genera numeri random
@@ -37,60 +38,62 @@ function genArrayRandomNum (arrMin, arrMax, arrLength) {
 }
 
 
-// GENERARE l'array di 5 numeri random tra 1 e 100
-const randomNumbers = genArrayRandomNum (1, 100, 5);
-console.log(`I numeri da ricordare sono: ${randomNumbers}`);
-
-
 // DICHIARAZIONE di 3 variabili: partenza del timer di 30 secondi, punteggio e i numeri utente
-let timer = 3;
-let score = 0;
+let timer = 30;
 let userNumber;
 
 
+// LINKARE il bottone "play" alla partenza del timer
+playBtn.addEventListener("click",
 
-// GENERARE un timer di 30 secondi, al terimne del quale i 5 numeri scompaiono
-const clock = setInterval(
-    function() {
-        if (timer === 0) {
+    function () {
 
-            clearInterval(clock);
+        // GENERARE l'array di 5 numeri random tra 1 e 100
+        const randomNumbers = genArrayRandomNum (1, 100, 5);
+        console.log(`I numeri da ricordare sono: ${randomNumbers}`);
 
-            alert("Tempo scaduto");
-
-            // i 5 numeri scompaiono, fare con d-none in html e css
-
-            // GENERARE un array contenente SOLO i numeri indovinati dall'utente
-            const userArray = [];
-            
-
-            // CHIEDERE all'utente i 5 numeri (con ciclo for)
-            for (let i = 0; i < 5; i++) {
-                
-                // PROMPT 
-                userNumber = parseInt(prompt("Inserisci un numero"));
-
-                // AGGIUNGERE i numeri all'array solo se presenti nell'array di numeri random
-                if (randomNumbers.includes(userNumber)) {
-                    userArray.push(userNumber);
-                }
-
-            } 
-            console.log(userArray);
-
-            if (userArray.length === 5) {
-                console.log("Hai vinto");
-            } else {
-                console.log(`Hai perso. Il tuo punteggio è stato di ${userArray.length} punti.`);
-            }
-
-            console.log(`Hai indovinato i seguenti numeri: ${user}`);
-
-        } else {
-            timer--
-            console.log(timer);
-        }
+        const clock = setInterval(
+            function() {
+                if (timer === 0) {
     
-    },
-    1000
+                    clearInterval(clock);
+    
+                    alert("Tempo scaduto");
+    
+                    // GENERARE un array contenente SOLO i numeri indovinati dall'utente
+                    const userArray = [];
+                    
+    
+                    // CHIEDERE all'utente i 5 numeri (con ciclo for)
+                    for (let i = 0; i < 5; i++) {
+                        
+                        // PROMPT 
+                        userNumber = parseInt(prompt("Inserisci un numero"));
+    
+                        // AGGIUNGERE i numeri all'array solo se presenti nell'array di numeri random
+                        if (randomNumbers.includes(userNumber)) {
+                            userArray.push(userNumber);
+                        }
+    
+                    } 
+                    console.log(userArray);
+    
+                    if (userArray.length === 5) {
+                        console.log("Hai vinto");
+                    } else {
+                        console.log(`Hai perso. Il tuo punteggio è stato di ${userArray.length} punti.`);
+                    }
+    
+                    console.log(`Hai indovinato i seguenti numeri: ${user}`);
+    
+                } else {
+                    timer--
+                    console.log(timer);
+                }
+            
+            },
+            1000
+        )
+    }
+    
 )
