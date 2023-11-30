2d61b89e8eb7473587d25b8262f813ab01be1dd8
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-30 16:21:21 +0100

    definite operazioni da svolgere js

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, i numeri che ha visto precedentemente.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+// FUNZIONE che genera numeri random
+
+
+
+// FUNZIONE che genera 5 numeri random in un dato range
+
+// GENERARE i 5 numeri
+
+// GENERARE un timer di 30 secondi, al terimne del quale i 5 numeri scompaiono
+
+// SELEZIONARE un form nell'html dove l'utnete inserisce i numeri
+
+// COMUNICARE quanti e quali numeri sono stati indovinati
+
